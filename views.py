b5dbd737c798e4bbeb9f5821708561ca11d8cd22
dumbo25 <dumbo25@users.noreply.github.com>
2018-03-06 18:42:27 -0600

    Create views.py

---
+++ views.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from app import app
+
+import json
+from collections import OrderedDict
+
+filename = "/home/pi/ha-hub/app/static/config/homepage.json"
+
+with open(filename, "r") as f:
+  allData = json.load(f, object_pairs_hook=OrderedDict)
+
+pageTitle = {'title', allData['title']}
+pageSideMenu = allData['side menu']
+
+
+@app.route('/')
+@app.route('/index/', methods=['GET', 'POST'])
+def homepage():
+    pageIcons = allData['pages']['Home']['icons']
+
+    templateData = {'page': 'Home', 'title': allData['title']}
+    return render_template('homepage.html', **templateData, pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/raspberrypi', methods=['GET', 'POST'])
+def raspberrypi():
+    pageIcons = allData['pages']['RaspberryPi']['icons']
+
+    templateData = {'page': 'RaspberryPi', 'title': allData['title']}
+    return render_template('homepage.html', **templateData, pageSideMenu=pageSideMenu, pageIcons=pageIcons)
