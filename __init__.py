545b3d18406ce1abdf39a804c66122050d1603ee
dumbo25 <dumbo25@users.noreply.github.com>
2018-12-29 20:15:30 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -29,6 +29,84 @@ def homepage():
     # return render_template('homepage.html', **templateData, pageSideMenu=pageSideMenu, pageIcons=pageIcons)
     return render_template('homepage.html', page='Home', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
 
+@app.route('/austin', methods=['GET', 'POST'])
+def austin():
+    pageIcons = allData['pages']['Austin']['icons']
+
+    return render_template('homepage.html', page='Austin', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/automation', methods=['GET', 'POST'])
+def automation():
+    pageIcons = allData['pages']['Automation']['icons']
+
+    return render_template('homepage.html', page='Automation', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/charities', methods=['GET', 'POST'])
+def charities():
+    pageIcons = allData['pages']['Charities']['icons']
+
+    return render_template('homepage.html', page='Charities', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/colorado', methods=['GET', 'POST'])
+def colorado():
+    pageIcons = allData['pages']['Colorado']['icons']
+
+    return render_template('homepage.html', page='Colorado', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/crowdfund', methods=['GET', 'POST'])
+def crowdfund():
+    pageIcons = allData['pages']['Crowd Fund']['icons']
+
+    return render_template('homepage.html', page='Crowd Fund', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/education', methods=['GET', 'POST'])
+def education():
+    pageIcons = allData['pages']['Education']['icons']
+
+    return render_template('homepage.html', page='Education', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/entertainment', methods=['GET', 'POST'])
+def entertainment():
+    pageIcons = allData['pages']['Entertainment']['icons']
+
+    return render_template('homepage.html', page='Entertainment', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/finance', methods=['GET', 'POST'])
+def finance():
+    pageIcons = allData['pages']['Finance']['icons']
+
+    return render_template('homepage.html', page='Finance', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/gardening', methods=['GET', 'POST'])
+def gardening():
+    pageIcons = allData['pages']['Gardening']['icons']
+
+    return render_template('homepage.html', page='Gardening', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/homediy', methods=['GET', 'POST'])
+def homediy():
+    pageIcons = allData['pages']['Home DIY']['icons']
+
+    return render_template('homepage.html', page='Home DIY', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/macbook', methods=['GET', 'POST'])
+def macbook():
+    pageIcons = allData['pages']['MacBook']['icons']
+
+    return render_template('homepage.html', page='MacBook', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/network', methods=['GET', 'POST'])
+def network():
+    pageIcons = allData['pages']['Network']['icons']
+
+    return render_template('homepage.html', page='Network', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/phone', methods=['GET', 'POST'])
+def phone():
+    pageIcons = allData['pages']['Phone']['icons']
+
+    return render_template('homepage.html', page='Phone', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
 @app.route('/raspberrypi', methods=['GET', 'POST'])
 def raspberrypi():
     pageIcons = allData['pages']['RaspberryPi']['icons']
@@ -37,6 +115,30 @@ def raspberrypi():
     # templateData = {'page': 'RaspberryPi', 'title': allData['title']}
     # return render_template('homepage.html', **templateData, pageSideMenu=pageSideMenu, pageIcons=pageIcons)
     return render_template('homepage.html', page='RaspberryPi', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/shopping', methods=['GET', 'POST'])
+def shopping():
+    pageIcons = allData['pages']['Shopping']['icons']
+
+    return render_template('homepage.html', page='Shopping', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/television', methods=['GET', 'POST'])
+def television():
+    pageIcons = allData['pages']['Television']['icons']
+
+    return render_template('homepage.html', page='Television', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/texas', methods=['GET', 'POST'])
+def texas():
+    pageIcons = allData['pages']['Texas']['icons']
+
+    return render_template('homepage.html', page='Texas', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
+
+@app.route('/wifi', methods=['GET', 'POST'])
+def wifi():
+    pageIcons = allData['pages']['Wi-Fi']['icons']
+
+    return render_template('homepage.html', page='Wi-Fi', title='My Home Page', pageSideMenu=pageSideMenu, pageIcons=pageIcons)
 
 
 if __name__ == "__main__":
